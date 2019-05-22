179a574d2971c294d042d0e47dd2ade132b71897
Xavi PC <xaverius00@gmail.com>
2019-05-22 16:00:19 -0500

    primera y segunda parte con firebase, servicio y cargador

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -11,22 +11,35 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada:boolean = false;
 
+  equipo: any[] = [];
+
   constructor(private http: HttpClient) {
 
-    console.log('Servicio de info página listo');
+    this.cargarInfo();
+    this.cargarEquipo();
     
+   }
+
+   private cargarInfo(){
+
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp:InfoPagina) =>{
       
-
-      // console.log(resp.twitter);
-
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
       
+    });
+   }
 
+   private cargarEquipo(){
+
+    this.http.get('https://angular-html-firebase.firebaseio.com/equipo.json')
+    .subscribe((resp:any[]) =>{
+      
+      this.equipo = resp
+      
     });
 
    }
+
 }
